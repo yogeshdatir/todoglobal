1ef967e6b5965095bdd9969a965ec4cb703589ee
yogeshdatir <yogdat@gmail.com>
2020-09-02 13:03:06 +0530

    Changing db config for dev and prod

--- todoglobal/settings.py
+++ todoglobal/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import dj_database_url
+import os
+import dj_database_url
+
+ON_HEROKU = os.environ.get('ON_HEROKU')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,14 +82,15 @@ WSGI_APPLICATION = 'todoglobal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if ON_HEROKU:
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600)    
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
-
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
